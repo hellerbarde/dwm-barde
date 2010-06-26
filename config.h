3ee3d4eb86abc7b6284a948af80d030e3e8b0578
phil <phil@localhost>
2010-06-26 16:37:10 +0200

    setting Mod4-q for killing clients

--- config.h
+++ config.h
@@ -80,7 +80,7 @@ static Key keys[] = {
 
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
